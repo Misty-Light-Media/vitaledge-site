c706f0d3b90b39637e612e2e8d774fe4448b9d51
Misty Light Media <misty.light.media@gmail.com>
2025-09-02 09:29:38 -0300

    Update Astro config for GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://andrew-drake.github.io/vitaledge',
+  site: 'https://misty-light-media.github.io/vitaledge-site/',
+  base: '/vitaledge-site/',
 });
